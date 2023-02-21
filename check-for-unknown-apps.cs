6928138e8790f302a68ea3d0849b5e4a63ae5c54
codepoet <jon.wise@jonandnic.com>
2023-02-21 10:38:58 -0500

    Update check-for-unknown-apps.cs

--- check-for-unknown-apps.cs
+++ check-for-unknown-apps.cs
@@ -1,29 +1,96 @@
-﻿//Check for Unknown Apps
+//Check for Unknown Apps
 //  Given an app list from the App Museum, generated from a directory listing
 //  AND given an inventory from a device, generated from the palm SDK
-//  (more details here: https://docs.webosarchive.org/appinventory/)
 //  Generate a list of apps on the device that don't exist in the App Museum
 
 //environment stuff
-var verbose = false;
+var verbose = true;
 var inventoryPath = @"c:\users\" + Environment.UserName + @"\Desktop\Check Apps\";
-var extantFile = "extant.txt";
-string[] skipPrefix = { "org.webosinternals", "ca.canucksoftware", "com.palm.app-museum2", "com.palm.app.calculator", "com.palm.calculator", "com.palm.app.contacts", "com.palm.quickoffice", "com.palm.app.camera", "com.palm.app.notes", "com.palm.app.email", "com.palm.app.searchpreferences", "com.palm.app.usbpassthrough", "com.palm.app.youtube", "com.palm.payment", "com.palm.app.photos", "com.palm.app.musicplayer", "com.palm.app.messaging", "com.palm.app.calendar", "com.palm.app.flashplugin", "com.palm.app.ondevlog", "com.yellowpages.ypmobile.preload", "com.palm.sysapp.voicedial", "com.palm.app.clock", "com.palm.app.phone", "com.palm.app.crotest" };
+
+string[] skipPrefix = { "com.palm.app-museum2", "com.palm.app.calculator", "com.palm.calculator", "com.palm.app.contacts", "com.palm.quickoffice", "com.palm.app.camera", "com.palm.app.notes", "com.palm.app.email", "com.palm.app.searchpreferences", "com.palm.app.usbpassthrough", "com.palm.app.youtube", "com.palm.payment", "com.palm.app.photos", "com.palm.app.musicplayer", "com.palm.app.messaging", "com.palm.app.calendar", "com.palm.app.flashplugin", "com.palm.app.ondevlog", "com.yellowpages.ypmobile.preload", "com.palm.sysapp.voicedial", "com.palm.app.clock", "com.palm.app.phone", "com.palm.app.crotest" };
+
+var skipFiles = new Dictionary<string, string>();
+skipFiles.Add("extantList", "ExtantApps.txt");
+skipFiles.Add("unmatchedList", "UnmatchedApps.txt");
+skipFiles.Add("prewareList", "PrewareApps.txt");
+skipFiles.Add("wantedList", "wanted.txt");
+
+string[] prewareUrls = { "http://ipkg.preware.net/feeds/webos-internals/all/Packages", "http://ipkg.preware.net/feeds/precentral/Packages", "http://ipkg.preware.net/feeds/precentral-themes/Packages" };
+var wantedUrl = "https://appcatalog.webosarchive.org/wanted.txt";
+
+List<string> knownApps = new List<string>();
+List<string> knownAppsExact = new List<string>();
+
+List<string> wantedApps = new List<string>();
+List<string> wantedAppsExact = new List<string>();
+
+List<string> matchWanted = new List<string>();
+
+List<string> cleanupCmds = new List<string>();
 
 //UI stuff
+Console.ForegroundColor = ConsoleColor.White;
 Console.WriteLine("webOS Catalog Compare");
 Console.WriteLine("=====================");
 Console.WriteLine();
 
 //read extant catalog
-//  generate from server like: ls > extant.txt
-var apps = File.ReadAllLines(inventoryPath + extantFile);
-for (var i = 0; i < apps.Length; i++)
-{
-    var line = apps[i].Split("_");  //remove extra info from appid in filename
-    apps[i] = line[0].Trim();
+//  generate from server like: ls > ExtantApps.txt
+if (File.Exists(Path.Combine(inventoryPath, skipFiles["extantList"]))) {
+    knownApps = readFileIntoInventory(inventoryPath, skipFiles["extantList"], knownApps, false);
+    knownAppsExact = readFileIntoInventory(inventoryPath, skipFiles["extantList"], knownAppsExact, true);
+    Console.WriteLine("Count of known apps now: " + knownApps.Count);
 }
-Console.WriteLine("Comparing to catalog of " + apps.Length + " apps...");
+else
+{
+    Console.WriteLine("Existing apps list does not exist!");
+    Console.WriteLine("Generate on server like: ls webshare/AppPackages > ../ExtantApps.txt");
+    Console.WriteLine("Then place in the working directory: " + inventoryPath);
+    Console.WriteLine();
+    Console.WriteLine("Press a key to proceed...");
+    Console.WriteLine();
+    Console.ReadKey();
+}
+
+//read Unmatched list
+//  generate from server like: ls > UnmatchedApps.txt
+if (File.Exists(Path.Combine(inventoryPath, skipFiles["unmatchedList"])))
+{
+    knownApps = readFileIntoInventory(inventoryPath, skipFiles["unmatchedList"], knownApps, false);
+    knownAppsExact = readFileIntoInventory(inventoryPath, skipFiles["unmatchedList"], knownAppsExact, true);
+    Console.WriteLine("Count of known apps now: " + knownApps.Count);
+    Console.WriteLine();
+}
+else
+{
+    Console.WriteLine("Unmatched apps list does not exist!");
+    Console.WriteLine("Generate on server like: ls webshare/Unmatched > ../UnmatchedApps.txt");
+    Console.WriteLine("Then place in the working directory: " + inventoryPath);
+    Console.WriteLine();
+    Console.WriteLine("Press a key to proceed...");
+    Console.WriteLine();
+    Console.ReadKey();
+}
+
+//read Preware catalog
+//  seperate tool generated this list from feeds, keep with this repo
+File.Delete(Path.Combine(inventoryPath, skipFiles["prewareList"]));
+foreach (string prewareFeed in prewareUrls)
+{
+    var useFileName = prewareFeed.Split('/')[4];
+    await getRemoteFile(prewareFeed, Path.Combine(Environment.GetEnvironmentVariable("TEMP"), useFileName));
+    await ParsePrewareFeed(Path.Combine(Environment.GetEnvironmentVariable("TEMP"), useFileName), Path.Combine(inventoryPath, skipFiles["prewareList"]));
+}
+knownApps = readFileIntoInventory(inventoryPath, skipFiles["prewareList"], knownApps, false);
+knownAppsExact = readFileIntoInventory(inventoryPath, skipFiles["prewareList"], knownAppsExact, true);
+Console.WriteLine("Count of known apps now: " + knownApps.Count);
+
+//read wanted list
+await getRemoteFile(wantedUrl, Path.Combine(inventoryPath, skipFiles["wantedList"]));
+Console.WriteLine(); 
+wantedApps = readFileIntoInventory(inventoryPath, skipFiles["wantedList"], wantedApps, false);
+wantedAppsExact = readFileIntoInventory(inventoryPath, skipFiles["wantedList"], wantedAppsExact, true);
+Console.WriteLine("Count of wanted apps now: " + wantedApps.Count);
 Console.WriteLine();
 
 //check each inventory file
@@ -31,7 +98,9 @@ Console.WriteLine();
 //  (manually remove any lines from text file that are not app listings)
 foreach (string file in Directory.EnumerateFiles(inventoryPath, "*.txt"))
 {
-    if (!file.Contains(extantFile))
+    //Console.WriteLine("Checking inventory: " + Path.GetFileName(file) + " contained? " + skipFiles.ContainsValue(Path.GetFileName(file)));
+    
+    if (!skipFiles.ContainsValue(Path.GetFileName(file)))
     {
         var contents = File.ReadAllLines(file);
         Console.ForegroundColor = ConsoleColor.White;
@@ -39,42 +108,137 @@ foreach (string file in Directory.EnumerateFiles(inventoryPath, "*.txt"))
 
         foreach (var line in contents)
         {
-            //clean up extra info in app listing
-            var skip = false;
-            var cleanline = line.Replace("*", "");
-            cleanline = cleanline.Trim();
-            var lineParts = cleanline.Split(' ');
-            cleanline = lineParts[0];
-            if (apps.Contains(cleanline))
+            if (line != "" && line.ToLower() != "_do not delete.txt" && line.ToLower() != "readme.md")
             {
-                if (verbose)
+                //clean up extra info in app listing
+                var skip = false;
+                var cleanline = line.Replace("*", "");
+                cleanline = cleanline.Trim();
+                var filename = cleanline;
+                var lineParts = cleanline.Split(' ');
+                cleanline = lineParts[0];
+                lineParts = cleanline.Split('_');
+                cleanline = lineParts[0];
+                var match = false;
+                var closeMatch = "";
+                if (knownApps.Contains(cleanline))
                 {
-                    Console.ForegroundColor = ConsoleColor.White;
-                    Console.WriteLine(cleanline + " found in Catalog");
-                }
-            }
-            else
-            {
-                foreach (var prefix in skipPrefix)  //skip well-known apps by prefix (defined above)
-                {
-                    if (cleanline.IndexOf(prefix) == 0)
+                    if (knownAppsExact.Contains(filename))
                     {
-                        skip = true;
                         if (verbose)
                         {
-                            Console.ForegroundColor = ConsoleColor.Cyan;
-                            Console.WriteLine("Skipping " + cleanline + " because prefix: " + prefix);
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.WriteLine("Exact match " + filename + " found in Catalog");
+                            cleanupCmds.Add("rm " + filename);
                         }
+                        match = true;
+                    }
+                    else
+                    {
+                        int index = knownAppsExact.FindIndex(str => str.Contains(cleanline));
+                        closeMatch = knownAppsExact[index];
                     }
                 }
-                if (!skip)
+                if (!match)
                 {
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.WriteLine(cleanline + " NOT found in Catalog");
+                    foreach (var prefix in skipPrefix)  //skip well-known apps by prefix (defined above)
+                    {
+                        if (cleanline.IndexOf(prefix) == 0)
+                        {
+                            skip = true;
+                            if (verbose)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Cyan;
+                                Console.WriteLine("Skipping " + cleanline + " because prefix: " + prefix);
+                            }
+                        }
+                    }
+                    if (!skip)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        var lineDone = false;
+                        Console.Write(line + " NOT previously known...");
+                        if (closeMatch != "")
+                        {
+                            Console.WriteLine(" Close match: " + closeMatch);
+                            lineDone = true;
+                            cleanupCmds.Add("mv " + filename + " ../OtherVersions");
+                        }
+                        if (wantedApps.IndexOf(cleanline) != -1)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.Write(" and WANTED: " + line);
+                            matchWanted.Add(line);
+                            Console.WriteLine();
+                            lineDone = true;
+                        }
+                        if (!lineDone)
+                            Console.WriteLine();
+                    }
                 }
             }
         }
         Console.ForegroundColor = ConsoleColor.White;
-        Console.WriteLine();
     }
+}
+
+Console.WriteLine();
+Console.WriteLine("New Matches from Wanted List: " + matchWanted.Count);
+Console.WriteLine("===============================");
+foreach (string app in matchWanted)
+{
+    Console.WriteLine("* " + app);
+}
+
+Console.WriteLine();
+Console.WriteLine("Suggested clean-ups: " + cleanupCmds.Count);
+Console.WriteLine("=======================");
+foreach (string cmd in cleanupCmds)
+{
+    Console.WriteLine(cmd);
+}
+
+List<string> readFileIntoInventory(string directory, string fileName, List<string> inv, bool exact)
+{
+    var contents = File.ReadAllLines(Path.Combine(directory, fileName));
+    if (!exact)
+        Console.WriteLine("Scanning inventory " + Path.Combine(directory, fileName) + " with " + contents.Length + " apps...");
+    for (var i = 0; i < contents.Length; i++)
+    {
+        var newVal = "";
+        if (exact)
+            newVal = contents[i];
+        else
+            newVal = contents[i].Split("_")[0];  //remove extra info from appid in filename
+        inv.Add(newVal.Trim());
+    }
+    return inv;
+}
+
+async Task getRemoteFile(string url, string filePath)
+{
+    Console.Write("Loading remote feed: " + Path.GetFileName(filePath));
+    using StreamWriter file = new(filePath);
+    var result = await new HttpClient().GetStringAsync(url);
+    await file.WriteAsync(result);
+}
+
+async Task ParsePrewareFeed(string inputPath, string outputPath) {
+    StreamWriter sw = new StreamWriter(outputPath, true);
+    var contents = File.ReadAllLines(inputPath);
+    var count = 0;
+    foreach (var line in contents)
+    {
+        if (line.IndexOf("Filename:") != -1)
+        {
+            //parse this line
+            var cleanLine = line.Substring(10, line.Length - 10);
+            //Console.WriteLine(cleanLine);
+            await sw.WriteLineAsync(cleanLine);
+            count++;
+        }
+    }
+    sw.Close();
+    Console.Write(" parsed " + count + " apps...");
+    Console.WriteLine();
 }
